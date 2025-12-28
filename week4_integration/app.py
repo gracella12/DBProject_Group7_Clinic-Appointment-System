1109468048618ae3c0774496d05aa25fe3f2cb15
alykhns <naliyakhairun@gmail.com>
2025-12-28 19:13:21 +0700

    try to deploy to vercel

--- week4_integration/app.py
+++ week4_integration/app.py
@@ -1,12 +1,12 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request, redirect, url_for, session, flash, g, current_app
 import pymysql
+# Melakukan simulasi agar library lain menganggap pymysql sebagai MySQLdb
 pymysql.install_as_MySQLdb()
-from flask_mysqldb import MySQL
-from werkzeug.security import generate_password_hash, check_password_hash
 import os
 from dotenv import load_dotenv
 from datetime import date, datetime, timedelta
-from MySQLdb.cursors import DictCursor 
+# Import DictCursor dari pymysql langsung
+from pymysql.cursors import DictCursor 
 
 load_dotenv()
 
@@ -19,8 +19,34 @@ app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
 app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 app.config['MYSQL_PORT'] = int(os.getenv('MYSQL_PORT', 3306))
 
+# --- PENGGANTI Flask-MySQLdb UNTUK VERCEL ---
+class MySQL:
+    def __init__(self, app=None):
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        @app.teardown_appcontext
+        def close_db(error):
+            db = g.pop('db', None)
+            if db is not None:
+                db.close()
+
+    @property
+    def connection(self):
+        if 'db' not in g:
+            g.db = pymysql.connect(
+                host=current_app.config['MYSQL_HOST'],
+                user=current_app.config['MYSQL_USER'],
+                password=current_app.config['MYSQL_PASSWORD'],
+                database=current_app.config['MYSQL_DB'],
+                port=current_app.config['MYSQL_PORT'],
+                autocommit=True
+            )
+        return g.db
 
 mysql = MySQL(app)
+# --------------------------------------------
 
 @app.route('/')
 def home():
@@ -33,32 +59,25 @@ def home():
         role = session.get('role')
         
         if role == 'resepsionis':
-            # KHUSUS RESEPSIONIS: Pindah ke Dashboard Baru
             return redirect(url_for('homepageResepsionis'))
             
         elif role == 'dokter':
-            # KHUSUS DOKTER: Pindah ke dashboard dokter (backend view)
             return redirect(url_for('dokter_dashboard'))
             
         elif role == 'pasien':
-            # PASIEN: Tetap di halaman utama tapi mode login
             cur = mysql.connection.cursor()
-            # Ambil nama depan pasien
             cur.execute("SELECT nama_depan FROM Pasien WHERE pasien_id = %s", (session.get('id'),))
             pasien_nama = cur.fetchone()
             cur.close()
             
             if pasien_nama:
-                # Set nama depan pasien ke session, menimpa nama dokter yang tersisa
                 session['nama_depan'] = pasien_nama[0]
 
             return render_template('home.html', email=session['email'], doctors=doctors)  
             
-        # Default jika role tidak dikenali
         return render_template('home.html', email=session['email'], doctors=doctors)
     
     else:   
-        # Jika belum login (Tamu)
         return render_template('home.html', doctors=doctors)
     
 
